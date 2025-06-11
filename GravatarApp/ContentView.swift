e498236302bcdfdbad19a60fdfcba94bdfe2167e
Eduardo Toledo <etoledom@icloud.com>
2025-06-11 16:38:42 +0200

    Adding Gravatar SDK dependency

--- GravatarApp/ContentView.swift
+++ GravatarApp/ContentView.swift
@@ -1,14 +1,40 @@
 import SwiftUI
+import Gravatar
 
 struct ContentView: View {
+    @State private var displayName: String = ""
+    @State private var email: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(alignment: .leading) {
+                Text("Gravatar email address:")
+                TextField("Email", text: $email)
+                    .textFieldStyle(.roundedBorder)
+                    .textContentType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+            }
+
+            Button("Fetch profile") {
+                fetchProfile()
+            }
+            .buttonStyle(.borderedProminent)
+            Text("Display Name:").padding(.top)
+            Text(displayName)
         }
         .padding()
+    }
+
+    func fetchProfile() {
+        let service = ProfileService()
+        Task {
+            do {
+                let profile = try await service.fetch(with: .email(email))
+                displayName = profile.displayName
+            } catch {
+                displayName = error.localizedDescription
+            }
+        }
     }
 }
 
