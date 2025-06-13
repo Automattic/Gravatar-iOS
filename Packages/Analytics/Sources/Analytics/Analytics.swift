bd32aa76083ce3a64d0b83ac85b86f70e18ee913
Eduardo Toledo <etoledom@icloud.com>
2025-06-13 09:56:22 +0200

    Move Tracker protocol to its own file

--- Packages/Analytics/Sources/Analytics/Analytics.swift
+++ Packages/Analytics/Sources/Analytics/Analytics.swift
@@ -1,34 +1,6 @@
 import Foundation
-import AutomatticTracks
+import AutomatticTracksEvents
 import OSLog
-
-public protocol Tracker {
-    func track(_ name: String, withCustomProperties: [String: AnyHashable])
-    func setUserProperty(_ value: AnyHashable, for key: String)
-    func configure()
-}
-
-extension TracksService: Tracker {
-    private enum Config {
-        static let prefix = "gravatar_ios"
-        static let userKey = "gravatar:user_id"
-        static let platform = "gravatar"
-    }
-
-    public func track(_ name: String, withCustomProperties: [String : AnyHashable]) {
-        trackEventName(name, withCustomProperties: withCustomProperties)
-    }
-
-    public func setUserProperty(_ value: AnyHashable, for key: String) {
-        userProperties[key] = value
-    }
-
-    public func configure() {
-        platform = Config.platform
-        eventNamePrefix = Config.prefix
-        authenticatedUserTypeKey = Config.userKey
-    }
-}
 
 @MainActor
 public class Analytics {
