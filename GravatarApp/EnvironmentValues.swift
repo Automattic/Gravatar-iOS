32868a07cb43916afa889f1a8a400454c550afa1
Eduardo Toledo <etoledom@icloud.com>
2025-06-16 12:16:09 +0200

    Using @Entry to define Analytics environment value

--- GravatarApp/EnvironmentValues.swift
+++ GravatarApp/EnvironmentValues.swift
@@ -3,18 +3,7 @@ import SwiftUI
 // MARK: - Analytics
 
 import Analytics
-extension EnvironmentValues {
-    var analytics: Analytics {
-        get {
-            self[AnalyticsKey.self]
-        }
-        set {
-            self[AnalyticsKey.self] = newValue
-        }
-    }
-}
 
-struct AnalyticsKey: EnvironmentKey {
-    @MainActor
-    static var defaultValue: Analytics { .init() }
+extension EnvironmentValues {
+    @Entry var analytics: Analytics = .init()
 }
