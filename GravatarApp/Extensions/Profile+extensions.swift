3edfaa10a92a2aa413fc815dd6572f7b7189d54a
Eduardo Toledo <etoledom@icloud.com>
2025-06-24 15:14:27 +0200

    Adding tab view

---
+++ GravatarApp/Extensions/Profile+extensions.swift
@@ -0,0 +1,25 @@
+#if DEBUG
+import Gravatar
+import Foundation
+
+extension Profile {
+    static var testProfile: Profile {
+        let rawProfile: [String: Any] = [
+            "hash":"hash",
+            "display_name": "John Appleseed",
+            "profile_url":"",
+            "avatar_url":"",
+            "avatar_alt_text":"",
+            "location":"",
+            "description":"",
+            "job_title":"",
+            "company":"",
+            "verified_accounts":[],
+            "pronunciation":"",
+            "pronouns":"",
+        ]
+        let rawData = try! JSONSerialization.data(withJSONObject: rawProfile, options: [])
+        return try! JSONDecoder().decode(Profile.self, from: rawData)
+    }
+}
+#endif
---
+++ GravatarApp/RootTabView.swift
@@ -0,0 +1,86 @@
+import Gravatar
+import SwiftUI
+
+struct RootTabView: View {
+    let profile: Profile
+    let onLogout: () -> Void
+
+    var body: some View {
+        TabView {
+            // MARK: - First tab
+            GravatarTab(profile: profile, onLogout: onLogout)
+
+            // MARK: - Second tab
+            ProfileTab(profile: profile, onLogout: onLogout)
+
+            // MARK: - Third tab
+            ShareTab()
+        }
+    }
+}
+
+struct GravatarTab: View {
+    let profile: Profile
+    let onLogout: () -> Void
+
+    var body: some View {
+        BackgroundColorView(color: .secondarySystemBackground) {
+            ContentView(profile: profile, onLogout: onLogout)
+        }
+        .tabItem {
+            Label("Gravatar", image: "gravatar-logo")
+        }
+    }
+}
+
+struct ProfileTab: View {
+    let profile: Profile
+    let onLogout: () -> Void
+
+    var body: some View {
+        BackgroundColorView(color: .secondarySystemBackground) {
+            ContentView(profile: profile, onLogout: onLogout)
+        }
+        .tabItem {
+            Label("Profile", systemImage: "brain.head.profile")
+        }
+    }
+}
+
+struct ShareTab: View {
+    var body: some View {
+        NavigationStack {
+            BackgroundColorView(color: .secondarySystemBackground) {
+                Text("Share!")
+            }
+            .navigationTitle("Share")
+            .navigationBarItems(
+                trailing: Button("Share", systemImage: "square.and.arrow.up", action: {})
+            )
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .background(Color(uiColor: .secondarySystemBackground))
+        .tabItem {
+            Label("Share", systemImage: "qrcode")
+        }
+    }
+}
+
+struct BackgroundColorView<Content>: View where Content: View {
+    let color: UIColor
+    let content: () -> Content
+
+    var body: some View {
+        ZStack {
+            Color(uiColor: color)
+                .ignoresSafeArea()
+            content()
+        }
+    }
+}
+
+
+
+ #Preview {
+     RootTabView(profile: Profile.testProfile, onLogout: {})
+ }
