5a30a09ee9085ff8ac477fa3c0d4c465c5e65473
etoledom <eduardo.toledo@automattic.com>
2025-06-25 13:37:33 +0200

    OAuth improvements (#11)

--- Packages/OAuth/Package.swift
+++ Packages/OAuth/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "OAuth",
     platforms: [
-        .iOS(.v16), .macOS(.v12), .visionOS(.v1),
+        .iOS("17.4"), .macOS(.v12), .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
