4814edb9284c24d9edec1ea94a64cf55bb20121c
Pinar Olguc <pinar.olguc@automattic.com>
2025-06-27 16:07:47 +0100

    Add dummy EditProfileViewModel

---
+++ GravatarApp/ProfileEditor/EditProfileViewModel.swift
@@ -0,0 +1,16 @@
+import Gravatar
+import SwiftUI
+
+@MainActor
+class EditProfileViewModel: ObservableObject {
+    private let authToken: String
+    private let profile: Profile
+
+    init(profile: Profile,
+         authToken: String) {
+        self.profile = profile
+        self.authToken = authToken
+    }
+    
+    // TODO: Implement
+}
