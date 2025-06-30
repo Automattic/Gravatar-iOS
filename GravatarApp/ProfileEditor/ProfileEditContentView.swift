7212fd294497aec8ca6d7dfa68162b7fd52f2ac1
Pinar Olguc <pinar.olguc@automattic.com>
2025-06-30 12:53:42 +0100

    Add constants

--- GravatarApp/ProfileEditor/ProfileEditContentView.swift
+++ GravatarApp/ProfileEditor/ProfileEditContentView.swift
@@ -3,6 +3,14 @@ import Gravatar
 import SwiftUI
 
 struct ProfileEditContentView: View {
+    private enum Constants {
+        static let primaryFont: Font = .subheadline
+        static let sectionHeaderFont: Font = .subheadline.weight(.semibold)
+        static let footerFont: Font = .footnote
+        static let horizontalPadding: CGFloat = .DS.Padding.double
+        static let vStackVerticalSpacing: CGFloat = .DS.Padding.medium
+    }
+
     @ObservedObject var viewModel: EditProfileViewModel
 
     var body: some View {
