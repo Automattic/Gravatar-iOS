e82a508a9c93de6d1471949ad0ba00cf2837307d
Pinar Olguc <pinar.olguc@automattic.com>
2025-07-08 15:37:02 +0100

    Add snapshot tests

---
+++ GravatarAppTests/ProfileTests/ProfileEditContentViewTests.swift
@@ -0,0 +1,24 @@
+import Foundation
+@testable import GravatarApp
+import SnapshotTesting
+import Testing
+
+@Suite(.snapshots(record: .failed, diffTool: .ksdiff))
+struct ProfileEditContentViewTests {
+    @MainActor
+    @Test
+    func testProfileEditContentViewIntrinsicHeight() async throws {
+        let editProfileViewModel: EditProfileViewModel = .init(userSession: .init(profile: .full, accessToken: "testToken"), urlSession: URLSessionProfileMock())
+        let view = ProfileEditContentView(viewModel: editProfileViewModel)
+            .fixedSize(horizontal: false, vertical: true)
+            .frame(width: ViewImageConfig.iPhone13Pro.size?.width ?? 0)
+        
+        assertSnapshots(
+            of: view,
+            as: [
+                .testStrategy(userInterfaceStyle: .light, layout: .sizeThatFits),
+                .testStrategy(userInterfaceStyle: .dark, layout: .sizeThatFits),
+            ]
+        )
+    }
+}
