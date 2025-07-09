397c29a14db4103b91ca25d0ddd5f9d423ceb731
Pinar Olguc <pinar.olguc@automattic.com>
2025-07-09 17:39:05 +0100

    Add snapshot tests

---
+++ GravatarAppTests/AvatarTests/AvatarPickerViewSnapshotTests.swift
@@ -0,0 +1,29 @@
+import Foundation
+@testable import GravatarApp
+import SnapshotTesting
+import Testing
+
+@Suite(.snapshots(record: .failed, diffTool: .ksdiff))
+struct AvatarPickerViewSnapshotTests {
+    @MainActor
+    @Test
+    func noAvatarSelected() async throws {
+        let viewModel = AvatarPickerViewModel(
+            userSession: UserSession(profile: .testProfile, accessToken: "token", context: .testContext),
+            urlSession: URLSessionMock()
+        )
+        await viewModel.fetchAvatars()
+        if let selectedAvatar = viewModel.grid.selectedAvatar {
+            _ = await viewModel.delete(selectedAvatar)
+        }
+        let view = AvatarPickerView(avatarPickerModel: viewModel, onLogout: {})
+            .frame(width: ViewImageConfig.iPhone13Pro.size?.width ?? 0, height: ViewImageConfig.iPhone13Pro.size?.height ?? 0)
+        assertSnapshots(
+            of: view,
+            as: [
+                .testStrategy(userInterfaceStyle: .light, layout: .sizeThatFits),
+                .testStrategy(userInterfaceStyle: .dark, layout: .sizeThatFits),
+            ]
+        )
+    }
+}
