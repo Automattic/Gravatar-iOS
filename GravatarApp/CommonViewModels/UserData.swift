dd5f907a7633cb6feee7bfd3982f33c00f619f66
Eduardo Toledo <etoledom@icloud.com>
2025-07-09 13:18:13 +0200

    Make `UserSession` listen to connectivity an update profile

--- GravatarApp/CommonViewModels/UserData.swift
+++ GravatarApp/CommonViewModels/UserData.swift
@@ -1,6 +1,7 @@
 import Combine
 import Gravatar
 import SwiftData
+import Foundation
 
 @MainActor
 class UserSession: ObservableObject {
@@ -9,10 +10,27 @@ class UserSession: ObservableObject {
 
     var context: ModelContext
 
-    init(profile: Profile, accessToken: String, context: ModelContext) {
+    private let networkMonitor: NetworkMonitor
+    private let profileService: ProfileService
+    private var cancellables = Set<AnyCancellable>()
+
+    init(profile: Profile, accessToken: String, context: ModelContext, networkMonitor: NetworkMonitor = SystemNetworkMonitor.shared, urlSession: URLSessionProtocol = URLSession.shared) {
         self.profile = profile
         self.accessToken = accessToken
         self.context = context
+        self.networkMonitor = networkMonitor
+        self.profileService = ProfileService(urlSession: urlSession)
+        setupCombine()
+    }
+
+    func setupCombine() {
+        networkMonitor.hasNetworkConnection.dropFirst().sink {[weak self] hasConnection in
+            guard let self, hasConnection else { return }
+            Task {
+                let profile = try await profileService.fetchOwnProfile(token: accessToken)
+                updateProfile(profile)
+            }
+        }.store(in: &cancellables)
     }
 
     func updateProfile(_ profile: Profile) {
