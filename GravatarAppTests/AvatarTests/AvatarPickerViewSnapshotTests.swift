c4a068fa5ee73771295395a1bc34bc960081ac59
Pinar Olguc <pinar.olguc@automattic.com>
2025-07-10 09:36:05 +0100

    Add a case for empty grid

--- GravatarAppTests/AvatarTests/AvatarPickerViewSnapshotTests.swift
+++ GravatarAppTests/AvatarTests/AvatarPickerViewSnapshotTests.swift
@@ -7,6 +7,24 @@ import Testing
 struct AvatarPickerViewSnapshotTests {
     @MainActor
     @Test
+    func emptyAvatarGrid() async throws {
+        let viewModel = AvatarPickerViewModel(
+            userSession: UserSession(profile: .testProfile, accessToken: "token", context: .testContext),
+            urlSession: URLSessionMock()
+        )
+        let view = AvatarPickerView(avatarPickerModel: viewModel, onLogout: {})
+            .frame(width: ViewImageConfig.iPhone13Pro.size?.width ?? 0, height: ViewImageConfig.iPhone13Pro.size?.height ?? 0)
+        assertSnapshots(
+            of: view,
+            as: [
+                .testStrategy(userInterfaceStyle: .light, layout: .sizeThatFits),
+                .testStrategy(userInterfaceStyle: .dark, layout: .sizeThatFits),
+            ]
+        )
+    }
+
+    @MainActor
+    @Test
     func noAvatarSelected() async throws {
         let viewModel = AvatarPickerViewModel(
             userSession: UserSession(profile: .testProfile, accessToken: "token", context: .testContext),
