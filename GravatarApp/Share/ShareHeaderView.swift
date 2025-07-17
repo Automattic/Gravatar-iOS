793be4f870de03ffcd464e8b3801d0e659e099ee
Eduardo Toledo <etoledom@icloud.com>
2025-07-17 14:59:18 +0200

    Basics for Share screen header

--- GravatarApp/Share/ShareHeaderView.swift
+++ GravatarApp/Share/ShareHeaderView.swift
@@ -2,86 +2,55 @@ import Gravatar
 import SwiftUI
 
 struct ShareHeaderView: View {
-    @EnvironmentObject var userSession: UserSession
+    let profile: Profile
+    let topPadding: CGFloat
+    let imageURL: URL?
 
     @Binding var forceRefresh: Bool
 
-    let qrGenerator: QRGenerator
-    @State var qrImage: UIImage?
-    @State var contentHeight: CGFloat = 0
-    @State var contentWidth: CGFloat = 0
-    @Binding var safeAreaInsets: EdgeInsets
-    let windowWidth: CGFloat
-    let maxHeight: CGFloat
+    @Environment(\.openURL) var openURL
 
-    private var imageURL: URL? {
-        AvatarURL(
-            with: .hashID(userSession.profile.hash),
-            options: .init(preferredSize: .pixels(Int(contentWidth / 2)))
-        )?.url
+    @State private var qrImage: UIImage?
+
+    private let qrGenerator: QRGenerator
+
+    @State private var buttonsSectionWidth: CGFloat = 0
+    @State private var windowWidth: CGFloat = 0
+
+    var qrWidth: CGFloat {
+        min(windowWidth - buttonsSectionWidth - 32 - 22, 350)
     }
 
-    init(
-        forceRefresh: Binding<Bool>,
-        profile: Profile,
-        safeAreaInsets: Binding<EdgeInsets>,
-        width: CGFloat,
-        maxHeight: CGFloat
-    ) {
+    init(profile: Profile, topPadding: CGFloat, imageURL: URL?, forceRefresh: Binding<Bool>) {
+        self.profile = profile
+        self.topPadding = topPadding
+        self.imageURL = imageURL
         self._forceRefresh = forceRefresh
+
         self.qrGenerator = QRGenerator(profile: profile)
-        self._safeAreaInsets = safeAreaInsets
-        self.windowWidth = width
-        self.maxHeight = maxHeight
     }
 
     var body: some View {
-        ZStack(alignment: .top) {
-            backgroundImageView(
-                imageURL: imageURL
-            )
-            .frame(height: contentHeight)
-            .clipped()
-            backgroundMaterial
-
-            VStack(spacing: 16) {
-                Text("Quickly share your public contact info by letting other scan this QR code.")
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(Color.white)
-                    .frame(width: windowWidth)
-                    .padding()
-
-                if let qrImage {
-                    Image(uiImage: qrImage)
-                        .resizable()
-                        .frame(
-                            maxWidth: min(windowWidth - 24, 400),
-                            maxHeight: min(windowWidth - 24, 400)
-                        )
-                        .scaledToFit()
-                        .cornerRadius(20)
-                        .padding(.bottom, 24)
-                } else {
-                    ProgressView()
+        BouncyImageBackgroundHeaderView(
+            topPadding: topPadding,
+            imageURL: imageURL,
+            forceRefresh: $forceRefresh
+        ) {
+            HStack(alignment: .top, spacing: 22) {
+                VStack(alignment:.leading, spacing: 16) {
+                    qrCode
+                    Text("Let others scan this QR code to share your contact information.")
+                    Text("buttonsWidth: \(buttonsSectionWidth)")
+                    Text("windowWidth: \(windowWidth)")
+                    Text("qrWidth: \(qrWidth)")
                 }
+                VStack(spacing: 8) {
+                    buttonsSection
+                }.contentWidthtReader($buttonsSectionWidth)
             }
-            .padding(.leading, safeAreaInsets.leading == 0 ? 24 : safeAreaInsets.leading)
-            .padding(.trailing, safeAreaInsets.trailing == 0 ? 24 : safeAreaInsets.trailing)
-            .padding(.top, safeAreaInsets.top == 0 ? 16 : safeAreaInsets.top)
-            .overlay(GeometryReader { geo in
-                Color.clear
-                    .onChange(of: geo.size) { _, newValue in
-                        contentHeight = newValue.height
-                    }
-            })
-            .frame(maxHeight: maxHeight)
+            .padding(.horizontal, 16)
         }
-        .overlay(GeometryReader { geo in
-            Color.clear
-                .onChange(of: geo.size) { _, newValue in
-                    contentWidth = newValue.width
-                }
-        })
+        .contentWidthtReader($windowWidth)
         .onAppear {
             Task {
                 qrImage = await qrGenerator.contactQRCode
@@ -89,38 +58,54 @@ struct ShareHeaderView: View {
         }
     }
 
-    private func backgroundImageView(imageURL: URL?) -> some View {
-        HeaderAvatarView(imageURL: imageURL, showLoading: false, forceRefresh: $forceRefresh) {
-            Color.clear
+    @ViewBuilder
+    var qrCode: some View {
+        if let qrImage {
+            Image(uiImage: qrImage)
+                .resizable()
+                .frame(
+                    width: qrWidth,
+                    height: qrWidth
+                )
+                .cornerRadius(8)
+        } else {
+            ProgressView()
         }
-        .scaledToFill()
-        .clipped()
     }
 
-    private var backgroundMaterial: some View {
-        Color.clear
-            .ignoresSafeArea()
-            .background(.ultraThinMaterial)
-            .frame(width: contentWidth, height: contentHeight)
-            .environment(\.colorScheme, .dark)
+    @ViewBuilder
+    var buttonsSection: some View {
+        Menu {
+            MainMenuOptions(profile: profile)
+        } label: {
+            EllipsisButton {}
+        }
+        CircularButton {} image: {
+            Image(.shareIcon)
+        }
+        CircularButton {} image: {
+            Image(.downloadIcon)
+        }
+        CircularButton {} image: {
+            Image(.enlargeIcon)
+        }
     }
 }
 
 #if DEBUG
 #Preview {
-    GeometryReader { geometry in
+    let imageURL = URL(string: "https://1.gravatar.com/avatar/1?size=256")
+    GeometryReader { geo in
         ScrollView {
             ShareHeaderView(
-                forceRefresh: .constant(false),
                 profile: .testProfile,
-                safeAreaInsets: .constant(geometry.safeAreaInsets),
-                width: geometry.size.width,
-                maxHeight: geometry.size.height
+                topPadding:
+                    geo.safeAreaInsets.top == 0 ?
+                16 : geo.safeAreaInsets.top,
+                imageURL: imageURL,
+                forceRefresh: .constant(false)
             )
-            .environmentObject(UserSession(profile: .testProfile, accessToken: "", context: .testContext))
-            .frame(width: geometry.size.width)
-        }
-        .ignoresSafeArea()
+        }.ignoresSafeArea()
     }
 }
 #endif
