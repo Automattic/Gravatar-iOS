e5cf737fde04bc02530c9c19c1dc251bbb23638b
Tanner W. Stokes <tanner.stokes@automattic.com>
2025-07-31 10:35:42 -0400

    Update script names.

--- .buildkite/commands/checkout_release_branch.sh
+++ .buildkite/commands/checkout_release_branch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eu
 
 # Script to checkout a specific release branch
-# Usage: ./checkout-release-branch.sh <RELEASE_VERSION>
+# Usage: ./checkout_release_branch.sh <RELEASE_VERSION>
 
 # Buildkite, by default, checks out a specific commit, ending up in a detached HEAD state.
 # But in some cases, we need to ensure to be checked out on the `release/*` branch instead, namely:
 # - When a `release-pipelines/*.yml` will end up needing to do a `git push` to the `release/*` branch (for version bumps)
-# - When doing a new `release-build.sh` from a job that was `pipeline upload`'d by such a pipeline,
+# - When doing a new `release_build.sh` from a job that was `pipeline upload`'d by such a pipeline,
 #   to ensure that the job doing the build would include that recent extra commit before starting doing the build.
 
 echo "--- :git: Checkout Release Branch"
