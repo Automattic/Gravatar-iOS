1c1e30fc8b7e982c7fb858dc818d56ef386dcb27
Pinar Olguc <pinar.olguc@automattic.com>
2025-08-12 18:31:29 +0100

    Add the events list

--- GravatarApp/AvatarPicker/AvatarPickerViewEvents.swift
+++ GravatarApp/AvatarPicker/AvatarPickerViewEvents.swift
@@ -1,8 +1,20 @@
-//
-//  AvatarPickerViewEvents.swift
-//  GravatarApp
-//
-//  Created by Pinar Olguc on 12.08.2025.
-//
-
+import Analytics
 import Foundation
+
+struct MenuEventProperties: Encodable & Sendable {
+    let screen: String
+}
+
+enum AvatarPickerViewEvents {
+    static let screen: String = "avatars"
+    static let screenView: AnalyticsEvent = ScreenViewEvent(screen: Self.screen)
+    static let screenLeave: AnalyticsEvent = ScreenLeaveEvent(screen: Self.screen)
+    static let mainMenuTapped: AnalyticsEvent = CommonAnalyticsEvent(name: "mainmenu_tapped", properties: MenuEventProperties(screen: Self.screen))
+    static let tabGravatarTapped: AnalyticsEvent = NoPropertyAnalyticsEvent(name: "tab_gravatar_tapped")
+    static let tabProfileTapped: AnalyticsEvent = NoPropertyAnalyticsEvent(name: "tab_profile_tapped")
+    static let tabQRTapped: AnalyticsEvent = NoPropertyAnalyticsEvent(name: "tab_qr_tapped")
+    static let avatarsGridItemTapped: AnalyticsEvent = NoPropertyAnalyticsEvent(name: "avatars_grid_item_tapped")
+    static let avatarsCameraButtonTapped: AnalyticsEvent = NoPropertyAnalyticsEvent(name: "avatars_camera_button_tapped")
+    static let avatarsPhotosButtonTapped: AnalyticsEvent = NoPropertyAnalyticsEvent(name: "avatars_photos_button_tapped")
+    static let avatarsAIButtonTapped: AnalyticsEvent = NoPropertyAnalyticsEvent(name: "avatars_ai_button_tapped")
+}
