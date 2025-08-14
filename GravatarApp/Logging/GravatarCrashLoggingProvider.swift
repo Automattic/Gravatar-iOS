7fedac2a5df911e5da7c42addfe446b8833355c8
Tanner W. Stokes <tanner.stokes@automattic.com>
2025-08-14 09:56:03 -0400

    Add basic Sentry crash logging.

---
+++ GravatarApp/Logging/GravatarCrashLoggingProvider.swift
@@ -0,0 +1,38 @@
+import AutomatticRemoteLogging
+
+actor GravatarCrashLogger {
+    static let shared = GravatarCrashLogger()
+    private let crashLogging: CrashLogging
+
+    private init() {
+        self.crashLogging = CrashLogging(
+            dataProvider: GravatarCrashLoggingDataProvider()
+        )
+    }
+
+    func start() {
+        do {
+            _ = try crashLogging.start()
+        } catch {
+            print("⚠️ Crash logging failed to start: \(error)")
+        }
+    }
+}
+
+struct GravatarCrashLoggingDataProvider: CrashLoggingDataProvider {
+    let sentryDSN = Secrets.sentryDSN
+    let userHasOptedOut = false
+
+    // TODO: Add current user
+    var currentUser: TracksUser? = nil
+
+    var buildType: String {
+        #if DEBUG
+        return "debug"
+        #elseif ENTERPRISE
+        return "prototype"
+        #else
+        return "release"
+        #endif
+    }
+}
